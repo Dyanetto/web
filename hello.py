d1682500b63bf431e2f2c0935a50904b8b9a6c78
Aleksandr Dyupachev <inetmail84@gmail.com>
2018-02-27 21:03:53 +0300

    Gunicorn added

---
+++ hello.py
@@ -0,0 +1,9 @@
+def app(env, start_response):
+  status = '200 ok'
+  headers = [('Content-Type', 'text/html')]
+  query_vars = env['QUERY_STRING'].split("&")
+  data = ""
+  for var in query_vars:
+    data += var + '\n' 
+  start_response(status, headers)
+  return [data.encode('utf-8')]
