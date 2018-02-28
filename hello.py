183d8f1942bd9b99f7017340fd37df039398bbdb
Aleksandr Dyupachev <inetmail84@gmail.com>
2018-02-28 15:37:13 +0300

    Gunicorn add

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def app(env, start_response):
   status = '200 ok'
-  headers = [('Content-Type', 'text/html')]
+  headers = [('Content-Type', 'text/plain']
   query_vars = env['QUERY_STRING'].split("&")
   data = ""
   for var in query_vars:
