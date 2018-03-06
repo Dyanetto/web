4df831b2be592d66a3a018ec52a788b4d113d3e3
Aleksandr Dyupachev <inetmail84@gmail.com>
2018-03-06 18:34:09 +0300

    Django

---
+++ ask/qa/urls.py
@@ -0,0 +1,12 @@
+from django.urls import re_path
+from ask.views import *
+
+urlpatterns = patterns('ask.views',
+    re_path(r'^$', test, name='root'),
+    re_path(r'^login/$', test, name='login'),
+    re_path(r'^signup/$', test, name='singup'),
+    re_path(r'^question/(?P<id>\d+)/$', test, name='question'),
+    re_path(r'^ask/$', test, name='ask'),
+    re_path(r'^popular/$', test, name='popular'),
+    re_path(r'^new/$', test, name='new'),
+)
